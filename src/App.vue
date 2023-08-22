23a0aa72a3ff72aae8520586b7bdf8fdd0ad6e96
GianluGiarde <gianlugiarde@gmail.com>
2023-08-22 12:51:44 +0200

    add style base

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script>
-  export default{
+export default {
 
-  }
+}
 </script>
 
 <template>
   <router-view></router-view>
-  <h1 class="text-danger">ciao</h1>
+  <h1>ciao</h1>
+  <font-awesome-icon icon="fa-solid fa-user-secret" />
+  <font-awesome-icon :icon="['fas', 'user-secret']" />
+  <font-awesome-icon :icon="['fas', 'user']" />
+  <font-awesome-icon :icon="['fab', 'facebook']" />
+  <font-awesome-icon :icon="['fab', 'instagram']" />
+  <font-awesome-icon :icon="['far', 'star']" />
 </template>
 
 <style lang="scss">
+@use './assets/styles/general.scss' as *;
 </style>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
-library.add(fas);
+library.add(fas, fab, far);
 
-/* add icons to the library */
-
-createApp(App).component ('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js";
