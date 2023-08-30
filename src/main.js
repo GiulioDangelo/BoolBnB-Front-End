3e5ec969143747ddece8a2148ddb55d447d1a609
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-08-30 11:06:57 +0200

    add app footer

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import { router } from "./router";
-import App from './App.vue'
+import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 library.add(fas, fab, far);
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
