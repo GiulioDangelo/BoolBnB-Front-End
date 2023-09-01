76dc346a2bda200b0d85552248582c044e7775c5
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-09-01 15:54:57 +0200

    add base style message

--- src/pages/AppMessage.vue
+++ src/pages/AppMessage.vue
@@ -1,7 +1,52 @@
-<template></template>
+<template>
+  <div class="container">
+    <h1>Invia un messaggio</h1>
+    <form @submit.prevent="inviaMessaggio">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          v-model="email"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <label for="descrizione" class="form-label">Descrizione</label>
+        <textarea
+          class="form-control"
+          id="descrizione"
+          v-model="descrizione"
+          required
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Invia</button>
+    </form>
+  </div>
+</template>
 
 <script>
-export default {};
-</script>
+export default {
+  data() {
+    return {
+      email: "",
+      descrizione: "",
+    };
+  },
+  methods: {
+    inviaMessaggio() {
+      // Qui puoi implementare la logica per inviare il messaggio, ad esempio tramite una chiamata API
+      const messaggio = {
+        email: this.email,
+        descrizione: this.descrizione,
+      };
+      console.log(messaggio);
 
-<style></style>
+      // Ripulisci i campi del form dopo l'invio
+      this.email = "";
+      this.descrizione = "";
+    },
+  },
+};
+</script>
