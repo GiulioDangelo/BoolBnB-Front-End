c21c1244ade27caefc15e90d4d36531099ef13fd
GianluGiarde <gianlugiarde@gmail.com>
2023-09-10 21:49:00 +0200

    add advanced search & contact validations

--- src/pages/AppApartmentShow.vue
+++ src/pages/AppApartmentShow.vue
@@ -23,63 +23,170 @@ export default {
 	},
 
 	methods: {
+
+		// validate & send form
+		sendMessage(event) {
+
+			if (!this.validateForm()) {
+				event.preventDefault();
+			} else {
+
+				// Verifica se è già in corso un invio
+				if (this.isSending) {
+					return;
+				}
+
+				this.isSending = true;
+
+				let data = {
+					email_sender: this.email_sender,
+					text_message: this.text_message,
+					apartment_id: this.apartment_id,
+				};
+
+				axios
+					.post(this.store.backendURL + "api/messages/store", data)
+					.then(response => {
+						this.isSending = false;
+
+						if (response.data.success) {
+							this.showSuccess = true;
+							this.resetForm();
+							this.reloadPage();
+						} else {
+							this.showError = true;
+							this.errors = response.data.errors;
+						}
+					})
+					.catch(error => {
+						this.isSending = false;
+						console.error(error);
+					});
+			}
+		},
+
+		// Reload the page
 		reloadPage() {
 			window.location.reload();
 		},
-		sendMessage() {
-			// Verifica se è già in corso un invio
-			if (this.isSending) {
-				return;
-			}
 
-			console.log("ciao");
-
-			this.isSending = true;
-
-			let data = {
-				email_sender: this.email_sender,
-				text_message: this.text_message,
-				apartment_id: this.apartment_id,
-			};
-
-			axios
-				.post(this.store.backendURL + "api/messages/store", data)
-				.then((response) => {
-					this.isSending = false;
-
-					if (response.data.success) {
-						this.showSuccess = true;
-						this.resetForm();
-						this.reloadPage();
-					} else {
-						this.showError = true;
-						this.errors = response.data.errors;
-					}
-				})
-				.catch((error) => {
-					this.isSending = false;
-					console.error(error);
-				});
-		},
-
-		// Ripulisci i campi del form dopo l'invio
+		// clear form data after submit
 		resetForm() {
 			this.email_sender = "";
 			this.text_message = "";
 			this.apartment_id = "";
+		},
+
+		// Validate all form when click
+		validateForm() {
+			let isValid = true;
+
+			const email = this.$refs.email;
+			const message = this.$refs.message;
+
+			if (this.email_sender.trim() === '') {
+				this.setError(email, 'Email richiesta');
+				isValid = false;
+			} else if (this.email_sender.trim().length < 5) {
+				this.setError(email, 'Minino 5 caratteri');
+				isValid = false;
+			} else if (this.email_sender.trim().length > 255) {
+				this.setError(email, 'Massimo 255 caratteri');
+				isValid = false;
+			} else if (!this.validateEmail(this.email_sender.trim())) {
+				this.setError(email, 'Email non valida');
+				isValid = false;
+			} else {
+				this.setSuccess(email);
+			}
+
+			if (this.text_message.trim() === '') {
+				this.setError(message, 'Messaggio richiesto');
+				isValid = false;
+			} else if (this.text_message.trim().length < 5) {
+				this.setError(message, `Minimo 5 caratteri`);
+				isValid = false;
+			} else {
+				this.setSuccess(message);
+			}
+
+			return isValid;
+		},
+
+		// Validate form on input
+		validate(event) {
+			const element = event.target;
+
+			switch (element.type) {
+				case "email":
+					if (this.email_sender.trim() === '') {
+						this.setError(element, 'Email richiesta');
+					} else if (this.email_sender.trim().length < 5) {
+						this.setError(element, 'Minino 5 caratteri');
+					} else if (this.email_sender.trim().length > 255) {
+						this.setError(element, 'Massimo 255 caratteri');
+					} else if (!this.validateEmail(this.email_sender.trim())) {
+						this.setError(element, 'Email non valida');
+					} else {
+						this.setSuccess(element);
+					}
+					break;
+
+				case "textarea":
+					if (this.text_message.trim() === '') {
+						this.setError(element, 'Messaggio richiesto');
+					} else if (this.text_message.trim().length < 5) {
+						this.setError(element, `Minimo 5 caratteri`);
+					} else {
+						this.setSuccess(element);
+					}
+					break;
+			}
+		},
+
+		// Validate email
+		validateEmail(email) {
+			const re =
+				/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(String(email).toLowerCase());
+		},
+
+		// Set error style
+		setError(element, message) {
+			const inputControl = element.parentElement;
+			const errorDisplay = inputControl.querySelector('.error');
+
+			errorDisplay.innerText = message;
+			inputControl.classList.add('error');
+			inputControl.classList.remove('success');
+		},
+
+		// Set success style
+		setSuccess(element) {
+			const inputControl = element.parentElement;
+			const errorDisplay = inputControl.querySelector('.error');
+
+			errorDisplay.innerText = '';
+			inputControl.classList.add('success');
+			inputControl.classList.remove('error');
+		},
+
+		// Prevent submit when click enter 
+		preventFormSubmitOnEnter(event) {
+			if (event.key === 'Enter') {
+				event.preventDefault();
+			}
 		},
 	},
 
 	created() {
 		axios
 			.get(this.store.backendURL + "api/apartments/" + this.$route.params.slug)
-			.then((response) => {
+			.then(response => {
 				this.apartment = response.data.results;
 				this.apartment_id = this.apartment.id;
 			})
-			.catch((error) => {
-				console.log(error);
-			});
+			.catch(error => console.log(error));
 	},
 };
 </script>
@@ -131,35 +238,43 @@ export default {
 			data-bs-whatever="@fat">
 			Contatta per Info
 		</button>
+
 		<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
-						<h1 class="modal-title fs-5" id="exampleModalLabel">
+						<h1 v-if="!showSuccess" class="modal-title fs-5" id="exampleModalLabel">
 							Nuovo Messaggio
 						</h1>
+						<h1 v-if="showSuccess" class="m-0">Messaggio Inviato</h1>
 						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
-					<div class="modal-body">
+					<div v-if="!showSuccess" class="modal-body">
 						<form @submit.prevent="sendMessage()">
-							<div class="mb-3">
-								<label class="col-form-label" for="email_sender">Email</label>
-								<input type="text" class="form-control" id="recipient-name" v-model="email_sender" />
+							<div class="input_container">
+								<label class="input_label text-gradient" for="email_sender">Email</label>
+								<font-awesome-icon :icon="['fas', 'envelope']" class="icon" />
+								<input type="email" class="input_field" id="email_sender" v-model="email_sender" ref="email"
+									@input="validate" @keydown.enter.prevent="preventFormSubmitOnEnter"
+									autocomplete="email" />
+								<div class="error"></div>
 							</div>
-							<div class="mb-3">
-								<label for="message-text" class="col-form-label">Descrizione</label>
-								<textarea class="form-control" id="message-text" v-model="text_message" for="text_message"
-									required></textarea>
+							<div class="input_container">
+								<label for="text_message" class="input_label text-gradient">Descrizione</label>
+								<textarea class="form-control" id="text_message" rows="3" v-model="text_message"
+									ref="message" @input="validate" @keydown.enter.prevent="preventFormSubmitOnEnter"
+									for="text_message"></textarea>
+								<div class="error"></div>
 							</div>
 							<input type="hidden" name="apartment_id" for="apartment_id" v-model="apartment_id" />
 						</form>
 					</div>
-					<div class="modal-footer">
+					<div class="modal-footer justify-content-between">
 						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-							Close
+							Chiudi
 						</button>
-						<button type="submit" class="styled-btn" @click="sendMessage">
-							Send message
+						<button v-if="!showSuccess" type="submit" class="styled-btn" @click="sendMessage">
+							Invia
 						</button>
 					</div>
 				</div>
