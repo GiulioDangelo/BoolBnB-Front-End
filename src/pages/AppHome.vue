44d2a34101bd8d3aedcc97ab9eb196d4f58c965e
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-09-11 10:52:50 +0200

    add appartamenti home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,9 +1,12 @@
 <script>
 import { store } from "../store";
 import axios from "axios";
+import apartmentList from "../components/ApartmentList.vue";
 
 export default {
-  components: {},
+  components: {
+    apartmentList,
+  },
 
   data() {
     return {
@@ -16,8 +19,8 @@ export default {
   mounted() {
     axios
       .get(this.store.backendURL + "api/apartments/")
-      .then(response => this.arrApartments = response.data.results)
-      .catch(error => console.error(error));
+      .then((response) => (this.arrApartments = response.data.results))
+      .catch((error) => console.error(error));
   },
   methods: {
     handleCarouselNavigation(direction) {
@@ -63,39 +66,57 @@ export default {
           </div>
           </div> -->
 
-
     <div id="carouselExampleCaptions" class="carousel slide">
       <!-- Carousel Items -->
       <div class="carousel-inner">
-
         <div v-for="apartment in arrApartments" :key="apartment.id">
           <div v-if="apartment.active_sponsors.length != 0">
-
-
-            <img :src="this.store.backendURL + 'storage/' + apartment.cover" alt="" class="caurosel-img" />
+            <img
+              :src="this.store.backendURL + 'storage/' + apartment.cover"
+              alt=""
+              class="caurosel-img"
+            />
             <div class="container-btn">
               <h5>{{ apartment.title }}</h5>
-              <router-link :to="{
-                name: 'apartments.show',
-                params: { slug: apartment.slug },
-              }" class="btn btn-primary mt-auto my-2">View more</router-link>
+              <router-link
+                :to="{
+                  name: 'apartments.show',
+                  params: { slug: apartment.slug },
+                }"
+                class="btn btn-primary mt-auto my-2"
+                >View more</router-link
+              >
             </div>
           </div>
         </div>
       </div>
     </div>
 
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev"
-      @click="handleCarouselNavigation('previous')">
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleCaptions"
+      data-bs-slide="prev"
+      @click="handleCarouselNavigation('previous')"
+    >
       <span class="carousel-control-prev-icon prev" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next"
-      @click="handleCarouselNavigation('next')">
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleCaptions"
+      data-bs-slide="next"
+      @click="handleCarouselNavigation('next')"
+    >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
+  </div>
+
+  <div class="container">
+    <h2 class="mt-5 py-5">I nostri appartmenti</h2>
+    <apartmentList />
   </div>
 
   <div class="container">
@@ -104,21 +125,36 @@ export default {
       <div class="col-6 city-container">
         <img
           src="https://cf.bstatic.com/xdata/images/city/600x600/613105.jpg?k=1e85cf4dec7b0d5a6327be91c38cf9c1711f9da1a31c4cba736f9cb751443ff1&o="
-          alt="" class="roma" />
+          alt=""
+          class="roma"
+        />
       </div>
       <div class="col-6 city-container">
-        <img src="https://hips.hearstapps.com/hmg-prod/images/napoli-1653914862.png" alt="" class="napoli" />
+        <img
+          src="https://hips.hearstapps.com/hmg-prod/images/napoli-1653914862.png"
+          alt=""
+          class="napoli"
+        />
       </div>
       <div class="col-4 city-container">
-        <img src="https://img.gruppomol.it/articoli/image/milano/1200x801_milano-mercato.jpg" alt="" class="milano" />
+        <img
+          src="https://img.gruppomol.it/articoli/image/milano/1200x801_milano-mercato.jpg"
+          alt=""
+          class="milano"
+        />
       </div>
       <div class="col-4 city-container">
         <img
           src="https://hips.hearstapps.com/hmg-prod/images/florence-royalty-free-image-1674549002.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*"
-          alt="" />
+          alt=""
+        />
       </div>
       <div class="col-4 city-container">
-        <img src="https://www.berlino.com/wp-content/uploads/sites/13/berlino-panorama.jpg" alt="" class="berlino" />
+        <img
+          src="https://www.berlino.com/wp-content/uploads/sites/13/berlino-panorama.jpg"
+          alt=""
+          class="berlino"
+        />
       </div>
     </div>
   </div>
